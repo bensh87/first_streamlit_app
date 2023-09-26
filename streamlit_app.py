9448ea05cb58d9a54291e7b370f0d0419edc3817
bensh87 <bensh87@gmail.com>
2023-09-26 18:41:29 +0530

    Update streamlit_app.py
    REading data from csv using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas   
 
 streamlit.title('YP Stores Welcomes you')
 
@@ -11,3 +12,7 @@ streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+my_food_list = pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+streamlit.data_frame(my_food_list)
