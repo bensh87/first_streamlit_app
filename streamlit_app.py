463741f99a34bb57ba53adaf8b04086da4372f66
bensh87 <bensh87@gmail.com>
2023-09-26 18:43:14 +0530

    Update streamlit_app.py
    dataframe Error

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,4 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_food_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.data_frame(my_food_list)
+streamlit.dataframe(my_food_list)
