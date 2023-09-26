dd374fca0ca7ff5a80a1efb1c0a68618fc9450ca
bensh87 <bensh87@gmail.com>
2023-09-26 18:29:56 +0530

    Update streamlit_app.py
    Adding Header and contents

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit
 
 streamlit.title('YP Stores Welcomes you')
 
-streamlit.header('Contents')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
