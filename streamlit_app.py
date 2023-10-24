b5b0f722321dd0490c0aaea2a77daa7e36225258
bensh87 <bensh87@gmail.com>
2023-10-24 22:40:50 +0530

    Snowflake Connector
    Snowflake Connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,16 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
-import snowflake.connector
-
 # write your own comment -Normalize the Text response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - To Display the data in table
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
+v
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
