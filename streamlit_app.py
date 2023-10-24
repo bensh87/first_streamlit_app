14e5296c0c9ff803420882dab8892868cc481900
bensh87 <bensh87@gmail.com>
2023-10-24 23:18:24 +0530

    Using try else block

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,21 +29,22 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 #User Input for Fruit
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
+
 streamlit.write('The user entered ', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# write your own comment -Normalize the Text response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - To Display the data in table
-streamlit.dataframe(fruityvice_normalized)
 #dont run anything past
 streamlit.stop()
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
