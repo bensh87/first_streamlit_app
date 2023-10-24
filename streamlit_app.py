021a132826e71d9362a8d13e971dd169eb46ecb2
bensh87 <bensh87@gmail.com>
2023-10-24 19:41:57 +0530

    Display API response in table
    Display API response in tablev

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,5 +29,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
+# write your own comment -Normalize the Text response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - To Display the data in table
+streamlit.dataframe(fruityvice_normalized)
